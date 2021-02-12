52280babb99180aa3ae996bd5450e65ec48f593d
Michael Esch <mail@michaelesch.de>
2021-02-12 18:51:05 +0100

    transform json to csv

--- src/de/eschsoftware/csv_json_transformator/main.go
+++ src/de/eschsoftware/csv_json_transformator/main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"github.com/tidwall/gjson"
+	"github.com/tushar2708/altcsv"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 type I18nData struct {
-	key string
+	key   string
 	value map[string]string
 }
+
+const FILE_PREFIX = "locale-"
 
 var wd string
 
@@ -27,24 +29,32 @@ func init() {
 func main() {
 	log.Println(wd)
 
-	files := getJsonAndCsvFiles(filepath.Join(wd, ".."))
+	files := getJsonFiles(filepath.Join(wd, ".."))
 	log.Println(files)
 	log.Println("---")
 	log.Println("---")
 	log.Println("---")
 
-	log.Println(readJsonFiles(&files))
+	model := readJsonFiles(&files, FILE_PREFIX)
+	log.Println(model)
 
+	headers := make([]string, 1)
+	headers[0] = "key"
+	for _, f := range files {
+		headers = append(headers, getLangCodeFromFilename(f, FILE_PREFIX))
+	}
+
+	writeCsv(filepath.Join(wd, "..", "i18n.csv"), headers, model)
 }
 
-func getJsonAndCsvFiles(path string) (resultFilenames []string) {
+func getJsonFiles(path string) (resultFilenames []string) {
 	infoList, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Panic(err)
 	}
 
 	for _, info := range infoList {
-		if filepath.Ext(info.Name()) == ".json" || filepath.Ext(info.Name()) == ".csv" {
+		if filepath.Ext(info.Name()) == ".json" {
 			fullFilename := filepath.Join(path, info.Name())
 			log.Println("found file: ", fullFilename)
 			resultFilenames = append(resultFilenames, fullFilename)
@@ -54,7 +64,7 @@ func getJsonAndCsvFiles(path string) (resultFilenames []string) {
 	return resultFilenames
 }
 
-func readJsonFiles(files *[]string) (resultMaps []I18nData) {
+func readJsonFiles(files *[]string, prefix string) (resultMaps []I18nData) {
 	for _, fullFilename := range *files {
 		if filepath.Ext(fullFilename) != ".json" {
 			continue
@@ -64,25 +74,76 @@ func readJsonFiles(files *[]string) (resultMaps []I18nData) {
 		if err != nil {
 			log.Panic("readJsonFiles error on ReadFile", fullFilename)
 		}
-		//var objmap map[string]json.RawMessage
-		//json.Unmarshal(fileContent, &objmap)
+
+		langCode := getLangCodeFromFilename(fullFilename, prefix)
 
 		parsedResult := gjson.ParseBytes(fileContent)
 		parsedResult.ForEach(func(key, value gjson.Result) bool {
-
+			log.Println(key)
+			build(&resultMaps, langCode, key.String(), value)
+			return true
 		})
 	}
 	return resultMaps
 }
 
-func build(i18nData *I18nData, langCode, previousKey string, obj gjson.Result) {
-		obj.ForEach(func(key, value gjson.Result) bool {
-			newKey := previousKey + "." + key.String()
-			if value.IsObject() || value.IsArray() {
-				build(i18nData, newKey, value)
+func getLangCodeFromFilename(fullFilename, prefix string) string {
+	baseFilename := filepath.Base(fullFilename)
+	return baseFilename[len(prefix) : len(baseFilename)-len(filepath.Ext(baseFilename))]
+}
+
+func build(i18nData *[]I18nData, langCode, previousKey string, obj gjson.Result) {
+	obj.ForEach(func(key, value gjson.Result) bool {
+		newKey := previousKey + "." + key.String()
+		log.Println(newKey)
+		if value.IsObject() || value.IsArray() {
+			build(i18nData, langCode, newKey, value)
+		} else {
+			index := findIndex(newKey, i18nData)
+			if index >= 0 {
+				// update
+				(*i18nData)[index].value[langCode] = value.String()
 			} else {
-
+				// add
+				m := make(map[string]string, 1)
+				m[langCode] = value.String()
+				*i18nData = append(*i18nData, I18nData{key: newKey, value: m})
 			}
-		})
+		}
+		return true
+	})
+}
 
+func findIndex(key string, i18nData *[]I18nData) int {
+	for i, data := range *i18nData {
+		if data.key == key {
+			return i
+		}
+	}
+	return -1
+}
+
+func writeCsv(exportFile string, headers []string, i18nData []I18nData) {
+	file, err := os.Create(exportFile)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+	w := altcsv.NewWriter(file)
+	w.Comma = ';'
+	w.AllQuotes = true
+	w.Quote = '"'
+	w.Write(headers)
+	for _, d := range i18nData {
+		line := make([]string, 1)
+		line[0] = d.key
+		for i, langCode := range headers {
+			if i == 0 {
+				continue
+			}
+			line = append(line, d.value[langCode])
+		}
+		w.Write(line)
+	}
+	w.Flush()
 }
