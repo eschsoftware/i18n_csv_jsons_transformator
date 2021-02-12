8c192756907909f97bc085874377e0db322a5edc
Michael Esch <mail@michaelesch.de>
2021-02-12 20:04:40 +0100

    transform csv to jsons

--- src/de/eschsoftware/csv_json_transformator/main.go
+++ src/de/eschsoftware/csv_json_transformator/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"github.com/mitchellh/mapstructure"
 	"github.com/tidwall/gjson"
 	"github.com/tushar2708/altcsv"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 )
 
 type I18nData struct {
@@ -27,24 +32,126 @@ func init() {
 }
 
 func main() {
-	log.Println(wd)
+	csvExportFilename := flag.String("csv-export", "i18n.csv", "filename for the CSV to export ")
+	toJson := flag.Bool("toJson", false, "transforms CSV file to different JSON files")
+	csvImportFilename := flag.String("csv-import", "i18n.csv", "filename of the CSV to import")
+	jsonFilePrefix := flag.String("json-file-prefix", FILE_PREFIX, "")
+	flag.Parse()
 
-	files := getJsonFiles(filepath.Join(wd, ".."))
-	log.Println(files)
-	log.Println("---")
-	log.Println("---")
-	log.Println("---")
+	if *toJson {
+		files := getJsonFiles(wd)
+		model := readJsonFiles(&files, *jsonFilePrefix)
+		headers := make([]string, 1)
+		headers[0] = "key"
+		for _, f := range files {
+			headers = append(headers, getLangCodeFromFilename(f, *jsonFilePrefix))
+		}
+		writeCsv(filepath.Join(wd, *csvExportFilename), headers, model)
+	} else {
+		headers, models := readCsv(csvImportFilename)
 
-	model := readJsonFiles(&files, FILE_PREFIX)
-	log.Println(model)
+		sort.Slice(models, func(i, j int) bool {
+			return models[i].key < models[j].key
+		})
 
-	headers := make([]string, 1)
-	headers[0] = "key"
-	for _, f := range files {
-		headers = append(headers, getLangCodeFromFilename(f, FILE_PREFIX))
+		log.Println(headers, models)
+
+		writeJsons(wd, *jsonFilePrefix, headers, models)
+	}
+}
+
+func writeJsons(wd string, prefix string, headers []string, models []I18nData) {
+	for i, header := range headers {
+		if i == 0 {
+			continue
+		}
+		fullFilename := filepath.Join(wd, prefix+header+".json")
+		writeJson(fullFilename, header, models)
+	}
+}
+
+func writeJson(filename string, header string, models []I18nData) {
+	jsonData := make(map[string]interface{})
+	for _, model := range models {
+		keyTokens := strings.Split(model.key, ".")
+
+		var childJsonData map[string]interface{}
+		if jsonData[keyTokens[0]] == nil {
+			childJsonData = make(map[string]interface{})
+		} else {
+			err := mapstructure.Decode(jsonData[keyTokens[0]], &childJsonData)
+			if err != nil {
+				log.Panic(err)
+			}
+		}
+
+		if len(keyTokens) > 1 {
+			jsonData[keyTokens[0]] = buildJson(childJsonData, keyTokens[1:], header, model)
+		} else {
+			jsonData[keyTokens[0]] = model.value[header]
+		}
+	}
+	data, err := json.Marshal(jsonData)
+	if err != nil {
+		log.Panic(err)
+	}
+	err = ioutil.WriteFile(filename, data, 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func buildJson(jsonData map[string]interface{}, keyTokens []string, header string, model I18nData) map[string]interface{} {
+	var childJsonData map[string]interface{}
+	if jsonData[keyTokens[0]] == nil {
+		childJsonData = make(map[string]interface{})
+	} else {
+		err := mapstructure.Decode(jsonData[keyTokens[0]], &childJsonData)
+		if err != nil {
+			log.Panic(err)
+		}
 	}
 
-	writeCsv(filepath.Join(wd, "..", "i18n.csv"), headers, model)
+	if len(keyTokens) > 1 {
+		jsonData[keyTokens[0]] = buildJson(childJsonData, keyTokens[1:], header, model)
+	} else {
+		jsonData[keyTokens[0]] = model.value[header]
+	}
+	return jsonData
+}
+
+func readCsv(filename *string) ([]string, []I18nData) {
+	file, err := os.Open(*filename)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer file.Close()
+
+	r := altcsv.NewReader(file)
+	r.Comma = ';'
+	r.Quote = '"'
+	content, err := r.ReadAll()
+	if err != nil {
+		log.Panic(err)
+	}
+	models := make([]I18nData, 0)
+	headers := content[0]
+	for i, line := range content {
+		if i == 0 {
+			continue
+		}
+		model := I18nData{key: line[0], value: map[string]string{}}
+		models = append(models, model)
+		for j, e := range line {
+			if j == 0 {
+				continue
+			}
+
+			model.value[headers[j]] = e
+		}
+	}
+
+	return headers, models
 }
 
 func getJsonFiles(path string) (resultFilenames []string) {
@@ -79,8 +186,7 @@ func readJsonFiles(files *[]string, prefix string) (resultMaps []I18nData) {
 
 		parsedResult := gjson.ParseBytes(fileContent)
 		parsedResult.ForEach(func(key, value gjson.Result) bool {
-			log.Println(key)
-			build(&resultMaps, langCode, key.String(), value)
+			buildModel(&resultMaps, langCode, key.String(), value)
 			return true
 		})
 	}
@@ -92,12 +198,12 @@ func getLangCodeFromFilename(fullFilename, prefix string) string {
 	return baseFilename[len(prefix) : len(baseFilename)-len(filepath.Ext(baseFilename))]
 }
 
-func build(i18nData *[]I18nData, langCode, previousKey string, obj gjson.Result) {
+func buildModel(i18nData *[]I18nData, langCode, previousKey string, obj gjson.Result) {
 	obj.ForEach(func(key, value gjson.Result) bool {
 		newKey := previousKey + "." + key.String()
 		log.Println(newKey)
 		if value.IsObject() || value.IsArray() {
-			build(i18nData, langCode, newKey, value)
+			buildModel(i18nData, langCode, newKey, value)
 		} else {
 			index := findIndex(newKey, i18nData)
 			if index >= 0 {
